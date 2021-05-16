7b1d41dc48f9486622729db4ec9202c066f5828e
Knot <knotto.vt@gmail.com>
2021-05-16 22:15:03 +0700

    add file

---
+++ RxImagePicker/RxImagePicker.h
@@ -0,0 +1,18 @@
+//
+//  RxImagePicker.h
+//  RxImagePicker
+//
+//  Created by Visarut Tippun on 16/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxImagePicker.
+FOUNDATION_EXPORT double RxImagePickerVersionNumber;
+
+//! Project version string for RxImagePicker.
+FOUNDATION_EXPORT const unsigned char RxImagePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxImagePicker/PublicHeader.h>
+
+
